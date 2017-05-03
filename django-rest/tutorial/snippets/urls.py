7133e710670f0b8212bcd56a4fa19305fe12c54f
Nerwin Hernando <nerwinc.hernando@gmail.com>
2017-05-04 00:09:22 +0800

    3_class_based_views

--- django-rest/tutorial/snippets/urls.py
+++ django-rest/tutorial/snippets/urls.py
@@ -2,11 +2,9 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
-urlpatterns = format_suffix_patterns([
-    url(r'^$', api_root),
-    url(r'^snippets/$', snippet_list, name='snippet-list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail, name='snippet-detail'),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', snippet_highlight, name='snippet-highlight'),
-    url(r'^users/$', user_list, name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail')
-])
+urlpatterns = [
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- django-rest/tutorial/tutorial/urls.py
+++ django-rest/tutorial/tutorial/urls.py
@@ -1,36 +1,7 @@
-"""tutorial URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-
+from django.contrib import admin
 from django.conf.urls import url, include
-from snippets import views
-from rest_framework.routers import DefaultRouter
-from rest_framework.schemas import get_schema_view
-schema_view = get_schema_view(title='Pastebin API')
 
-# Create a router and register our viewsets with it.
-router = DefaultRouter()
-router.register(r'snippets', views.SnippetViewSet)
-router.register(r'users', views.UserViewSet)
-
-# The API URLs are now determined automatically by the router.
-# Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('snippets.urls')),
 ]
-
-
-urlpatterns += [url(r'^schema/$', schema_view), ]
